927d740da660560278c470236667d819c4122af8
answjdgus72 <answjdgus72@gmail.com>
2024-11-14 13:29:50 +0900

    최종

--- chap03-view-resolver-leture-source/src/main/java/com/ohgiraffers/viewresolver/ResolverController.java
+++ chap03-view-resolver-leture-source/src/main/java/com/ohgiraffers/viewresolver/ResolverController.java
@@ -1,5 +1,4 @@
-package com.ohgiraffers.viewresolver;
-
+package com.ohgiraffers.rviewresolver;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,33 +13,37 @@ public class ResolverController {
      *   핸들러 메소드에서 마지막으로 해야 할 일은
      *   client 에게 응답해야 할 페이지를 리턴하는 것이다.
      *   스프링에서는 다양한 전략에 맞게 뷰를 해석할 수 있는
-     *   viewResolver 구현체가 존재한다.
+     *   ViewResolver 구현체가 존재한다.
      *   우리가 Thymeleaf 의존성을 추가하게 된다면
      *   뷰리졸버는 타임리프 문법을 해석할 수 있는
      *   ThymeleafViewResolver 로 전환이 되며
      *   이는 prefix 로 resource/templates/
-     *   suffix 로 .html 을 자동으로 붙여주게 된다.*/
-    @GetMapping("string")
-    public String stringReturn(Model model){
+     *   suffix 로 .html 을 자동으로 붙여주게 된다.
+     *  */
 
-        model.addAttribute("forwardMessage",
-                           "문자열로 view 반환" );
+    @GetMapping("string")
+    public String stringReturn(Model model) {
+
+        model.addAttribute("forwardMessage"
+                , "문자열로 view 반환");
 
         return "result";
     }
-        @GetMapping("string-redirect")
-        public String stringRedirect() {
+
+    @GetMapping("string-redirect")
+    public String stringRedirect() {
 
         /* comment.
-        *   우리가 지금까지 view 를 반환하는 방식은
-        *   default 로 forward 방식이다.
-        *   redirect 를 하는 방법은 접두사로 redirect: 를 붙이면 된다.
-        *   */
-                return "redirect:/";
-        }
+         *   우리가 지금까지 view 를 반환하는 방식은
+         *   default 로 forward 방식이다.
+         *   redirect 를 하는 방법은
+         *   접두사로 redirect: 를 붙이면 된다.
+         *  */
+        return "redirect:/";
+    }
 
-    /*comment.
-     *  redirect 란, 최초 요청을 받게 되면 응답을 진행 후
+    /* comment.
+     *   redirect 란, 최초 요청을 받게 되면 응답을 진행 후
      *   다시 재요청을 하는 방식이다.
      *   따라서 request 가 2번 일어나기 때문에 값을 유지
      *   할 방법이 없다.
@@ -50,35 +53,45 @@ public class ResolverController {
      *   관리 할 데이터가 많아지면 서버의 부담이 생길 수 있다.
      *   스프링에서는 RedirectAttributes 라는 타입을 통해
      *   redirect 시 값을 저장할 수 있는 기능을 제공해준다.
-     * */
-        @GetMapping("string-redirect-attr")
-    public String redirectAttr(RedirectAttributes attr){
+     *  */
+    @GetMapping("string-redirect-attr")
+    public String redirectAttr(RedirectAttributes attr) {
 
-           attr.addFlashAttribute("flashMessage",
-                                 "리다이렉트 시 값 유지!!");
+        /* comment.
+         *   RedirectAttributes 는 Session 의 기능을 사용하지만,
+         *   Flash -> 잠깐 값이 담겼다가 소멸되는 방식을 사용한다.
+         *   Session 처럼 소멸 될 때까지 공간을 차지하는 것이 아닌
+         *   잠시 데이터를 썼다가 사라지는 구조이기 때문에 서버의 부담이 없다.
+         *  */
+        attr.addFlashAttribute("flashMessage",
+                "리다이랙트 시 값 유지!!");
 
-            return "redirect:/";
-        }
+        return "redirect:/";
+    }
 
-        @GetMapping("modelandview")
-        public ModelAndView modelAndViewReturn(ModelAndView mv){
-            mv.addObject("forwardMessage",
-                            "ModelAndView 를 이용해서 반환");
-            mv.setViewName("result");
+    /* comment.
+     *   Model 과 View 를 합친 개념이다.
+     *   값을 집어넣을 수도 있고, 화면을 결정 지을 수도 있다.
+     *  */
+    @GetMapping("modelandview")
+    public ModelAndView modelAndViewReturn(ModelAndView mv) {
+        // Model 객체처럼 화면에 쓰일 데이터 넣을 수 있다.
+        mv.addObject("forwardMessage",
+                "ModelAndView 를 이용해서 반환");
 
-            return mv;
-        }
+        // 문자열로 이동하고 싶은 view 페이지를 지정할 수 있다.
+        mv.setViewName("result");
 
-        @GetMapping("modelandview-redirect-attr")
+        return mv;
+    }
+
+    @GetMapping("modelandview-redirect-attr")
     public ModelAndView mvRedirectAttr(ModelAndView mv, RedirectAttributes attr) {
 
-            attr.addFlashAttribute("flashMessage2",
-                    "ModelAndView 리다이렉트 시 값유지!!!");
-            mv.setViewName("redirect:/");
+        attr.addFlashAttribute("flashMessage2",
+                "ModelAndView 리다이랙트 시 값 유지!!!");
+        mv.setViewName("redirect:/");
 
-            return mv;
-
-        }
-
-
+        return mv;
+    }
 }
--- chap04-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exception/GlobalTestController.java
+++ chap04-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exception/GlobalTestController.java
@@ -1,16 +1,45 @@
+
 package com.ohgiraffers.exception;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller()
+@Controller
 public class GlobalTestController {
 
+    /* comment.
+     *   다른 클래스에서 @ExceptionHandler 메소드를
+     *   작성해두었다고 해서 지금 클래스에서 발생하는
+     *   예외를 처리할 수는 없다.
+     *   WHY? -> 전혀 연관 없는 클래스이기 때문에
+     *  */
     @GetMapping("global-nullpointer")
     public String globalNull() {
+
         String str = null;
         System.out.println(str.charAt(0));
 
         return "/";
     }
+
+    @GetMapping("global-userexception")
+    public String globalUserException() throws MemberNotFoundException {
+        boolean check = true;
+        if(check) {
+            throw new MemberNotFoundException("찾는 회원이 없어...");
+        }
+        return "/";
+    }
+
+    @GetMapping("global-default")
+    public String manyException() {
+        double[] array = new double[0];
+        // 배열의 크기를 0으로 만들어두고
+        // 1번째 데이터 출력하려고 할 때
+        System.out.println(array[0]);
+
+        return "/";
+    }
+
 }
