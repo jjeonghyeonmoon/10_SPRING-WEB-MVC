527d99ce98267336ae21acdf0646675d78fe17f4
answjdgus72 <answjdgus72@gmail.com>
2024-11-18 15:11:33 +0900

    no message

--- chap06-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileController.java
+++ chap06-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileController.java
@@ -12,6 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -73,14 +75,60 @@ public class FileController {
         System.out.println("savedName = " + savedName);
 
         /* index. 3. 고유한 파일 식별번호 및 파일 저장경로 생성 완료.
-        *   이제 파일을 저장경로에 저장
-        * */
-
+         *   이제 파일을 저장경로에 저장
+         *  */
         // 전달 받은 파일 객체를 변환
         singleFile.transferTo(new File(filePath + "/" + savedName));
 
-        model.addAttribute("message","파일 업로드 성공!!");
-        model.addAttribute("img","static/img/single/" + savedName);
+        model.addAttribute("message", "파일 업로드 성공!!");
+        model.addAttribute("img", "static/img/single/" + savedName);
+
+        return "result";
+    }
+
+    @PostMapping("multi-file")
+    public String multiFile(@RequestParam List<MultipartFile> multiFile,
+                            String description, Model model) throws IOException {
+
+        /* index. 1. 파일을 저장할 위치 설정 */
+        Resource resource =
+                resourceLoader.getResource("classpath:static/img/multi");
+
+        String filePath = null;
+
+        // 위에서 지정한 파일 저장 위치가 존재하지 않는다면
+        if(!resource.exists()) {
+            String root = "src/main/resources/static/img/multi";
+            File file = new File(root);
+            // make directory 의 약자
+            // 우리가 root 에 지정한 경로대로 디랙토리를 만들어준다.
+            file.mkdirs();
+
+            // 만든 폴더의 경로를 filePath 변수에 담아주기
+            filePath = file.getAbsolutePath();
+        } else {
+            // 디렉토리가 만들어진 적이 있다면
+            filePath = resourceLoader.getResource("classpath:static/img/multi")
+                    .getFile().getAbsolutePath();
+        }
+
+        /* index. 2. 멀티 파일 변경 처리 */
+        List<FileDTO> files = new ArrayList<>();
+        List<String> savedFiles = new ArrayList<>();
+
+        for(MultipartFile file : multiFile) {
+
+            String originFileName = file.getOriginalFilename();
+            String ext = originFileName.substring(originFileName.lastIndexOf("."));
+            String savedName = UUID.randomUUID().toString().replace("-", "") + ext;
+
+            files.add(new FileDTO(originFileName, savedName, filePath, description));
+            file.transferTo(new File(filePath + "/" + savedName));
+            savedFiles.add("static/img/multi/" + savedName);
+        }
+
+        model.addAttribute("message", "파일 업로드 성공!!");
+        model.addAttribute("imgs", savedFiles);
 
         return "result";
     }
