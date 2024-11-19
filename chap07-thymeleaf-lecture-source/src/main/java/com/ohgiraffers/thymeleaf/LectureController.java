07feafbb4d093b0b6e5c5c125d8b572266ce0010
answjdgus72 <answjdgus72@gmail.com>
2024-11-19 10:52:07 +0900

    1차

---
+++ chap07-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/LectureController.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/lecture/*")
+public class LectureController {
+
+    @GetMapping("expression")
+    public ModelAndView expression(ModelAndView mv) {
+
+        mv.addObject("member",new MemberDTO("조평훈",20,'남',"경기도수원시"));
+
+        mv.setViewName("lecture/expression");
+
+        return mv;
+    }
+}
